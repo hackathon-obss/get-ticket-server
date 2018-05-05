2c907726d3a554fff54d169d9066c56b9d75eda9
Ekrem Talha Selamet <eselamet@etu.edu.tr>
2018-05-06 02:35:04 +0300

    classifier name changed

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['TESTING'] = True
 
-clf = None
+timeClf = None
+subeClf = None
 users = {}
 user_sube = {}
 
@@ -28,7 +29,7 @@ def add_user():
     eta2 = request.form['eta2']
     operation = request.form['operation']
     users[uid] = User(uid, eta1, eta2, operation)
-    sube = str(clf.predict(array([[int(eta1), int(eta2), int(operation), sube_total_time(1), sube_total_time(2)]]))[0])
+    sube = str(subeClf.predict(array([[int(eta1), int(eta2), int(operation), sube_total_time(1), sube_total_time(2)]]))[0])
     user_sube[uid] = UserSube(uid, sube, operation)
     return 'sube:' + sube
 
