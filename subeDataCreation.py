2be4e5bd78b9a825c61f601446ea5881ea0f4e18
Ekrem Talha Selamet <eselamet@etu.edu.tr>
2018-05-05 18:03:01 +0300

    subedatacreation

--- subeDataCreation.py
+++ subeDataCreation.py
@@ -4,6 +4,9 @@ import sklearn
 from sklearn.neural_network import MLPClassifier
 from numpy import array
 islemTipleri = [0,5,10,15,20]
+sube1user = []
+sube2user = []
+trainData = []
 
 def append_to_file(file_name, text):
 	f = open(file_name,"a")
@@ -35,71 +38,109 @@ def getSubeEstimationTime(lst):
         time = time + islemTipleri[item[2]]
     return time
 
-sube1user = []
-sube2user = []
-trainData = []
-"""
-print_to_new_file("allData.txt","")
+def trainSubeData():
+    print_to_new_file("allData.txt","")
 
-for i in range(1000):
-    sube1arriveTime = random.randint(0,15)
-    sube2arriveTime = random.randint(0,15)
-    islemTipi = random.randint(1,4)
-    sube1totalTime = getSubeEstimationTime(sube1user)
-    sube2totalTime = getSubeEstimationTime(sube2user)
-    subeSecimi = 0
-    if sube1totalTime+islemTipleri[islemTipi] <= sube2totalTime+islemTipleri[islemTipi]:
-        sube1user.append([sube1arriveTime, sube2arriveTime, islemTipi, sube1totalTime+sube2totalTime])
-        subeSecimi = 1
-    else:
-        sube2user.append([sube1arriveTime, sube2arriveTime, islemTipi, sube2totalTime])
-        subeSecimi = 2
-    trainData.append([sube1arriveTime, sube2arriveTime, islemTipi, sube1totalTime, sube2totalTime, subeSecimi])
-print("train data creation has ended")
-for dt in trainData:
-    append_to_file("allData.txt", str(dt))"""
-lines = read_lines_from_file("allData1.txt")
-for line in lines:
-    line = line.replace("[","").replace("]","").replace(" ","").replace("\n","").split(",")
-    trainData.append([int(line[0]),int(line[1]),int(line[2]),int(line[3]),int(line[4]),int(line[5])])
+    for i in range(1000):
+        sube1arriveTime = random.randint(0,15)
+        sube2arriveTime = random.randint(0,15)
+        islemTipi = random.randint(1,4)
+        sube1totalTime = getSubeEstimationTime(sube1user)
+        sube2totalTime = getSubeEstimationTime(sube2user)
+        subeSecimi = 0
+        if sube1totalTime+islemTipleri[islemTipi] <= sube2totalTime+islemTipleri[islemTipi]:
+            sube1user.append([sube1arriveTime, sube2arriveTime, islemTipi, sube1totalTime+sube2totalTime])
+            subeSecimi = 1
+        else:
+            sube2user.append([sube1arriveTime, sube2arriveTime, islemTipi, sube2totalTime])
+            subeSecimi = 2
+        trainData.append([sube1arriveTime, sube2arriveTime, islemTipi, sube1totalTime, sube2totalTime, subeSecimi])
+    print("train data creation has ended")
+    for dt in trainData:
+        append_to_file("allData.txt", str(dt))
+    """lines = read_lines_from_file("allData1.txt")
+    for line in lines:
+        line = line.replace("[","").replace("]","").replace(" ","").replace("\n","").split(",")
+        trainData.append([int(line[0]),int(line[1]),int(line[2]),int(line[3]),int(line[4]),int(line[5])])
+    """
+    values = []
+    result = []
+    for item in trainData:
+        values.append([item[0],item[1],item[2],item[3],item[4]])
+        result.append(item[5])
+    trainValuesNpArray = array(values)
+    trainResultNpArray = array(result)
+    _solver = 'lbfgs'
+    _alpha = 1e-5
+    _hiddenLayerSize = 	(50,50,)
+    clf = MLPClassifier(solver=_solver, alpha=_alpha, hidden_layer_sizes=_hiddenLayerSize)
+    clf.fit(trainValuesNpArray, trainResultNpArray)
+    print("train has ended")
+    """predicted = clf.predict(array([[2, 3, 3, 0, 0]]))
+    print(predicted)
+    predicted = clf.predict(array([[15, 0, 1, 15, 0]]))
+    print(predicted)
+    predicted = clf.predict(array([[0, 0, 4, 15, 5]]))
+    print(predicted)
+    predicted = clf.predict(array([[13, 6, 1, 15, 25]]))
+    print(predicted)
+    predicted = clf.predict(array([[5, 10, 2, 20, 25]]))
+    print(predicted)
 
-values = []
-result = []
-for item in trainData:
-    values.append([item[0],item[1],item[2],item[3],item[4]])
-    result.append(item[5])
-trainValuesNpArray = array(values)
-trainResultNpArray = array(result)
-_solver = 'lbfgs'
-_alpha = 1e-5
-_hiddenLayerSize = 	(50,50,)
-clf = MLPClassifier(solver=_solver, alpha=_alpha, hidden_layer_sizes=_hiddenLayerSize)
-clf.fit(trainValuesNpArray, trainResultNpArray)
-print("train has ended")
-predicted = clf.predict(array([[2, 3, 3, 0, 0]]))
-print(predicted)
-predicted = clf.predict(array([[15, 0, 1, 15, 0]]))
-print(predicted)
-predicted = clf.predict(array([[0, 0, 4, 15, 5]]))
-print(predicted)
-predicted = clf.predict(array([[13, 6, 1, 15, 25]]))
-print(predicted)
-predicted = clf.predict(array([[5, 10, 2, 20, 25]]))
-print(predicted)
+    predicted = clf.predict(array([[8, 5, 4, 30, 25]]))
+    print(predicted)
+    predicted = clf.predict(array([[4, 5, 4, 30, 45]]))
+    print(predicted)
+    predicted = clf.predict(array([[13, 9, 1, 50, 45]]))
+    print(predicted)
+    predicted = clf.predict(array([[1, 12, 1, 50, 50]]))
+    print(predicted)
+    predicted = clf.predict(array([[4, 14, 2, 55, 50]]))
+    print(predicted)"""
 
-predicted = clf.predict(array([[8, 5, 4, 30, 25]]))
-print(predicted)
-predicted = clf.predict(array([[4, 5, 4, 30, 45]]))
-print(predicted)
-predicted = clf.predict(array([[13, 9, 1, 50, 45]]))
-print(predicted)
-predicted = clf.predict(array([[1, 12, 1, 50, 50]]))
-print(predicted)
-predicted = clf.predict(array([[4, 14, 2, 55, 50]]))
-print(predicted)
+def calculateSubeValue(lst, subeNo):
+    return lst[subeNo-1] + islemTipleri[lst[2]]
 
+def subeSiraValueHesapla(subeList, subeNo):
+    newSubeList = []
+    count = 0
+    for item in subeList:
+        count = count + 1
+        #newSubeList.append([item[subeNo-1], islemTipleri[item[2]], calculateSubeValue(item, subeNo)])
+        newSubeList.append([item[subeNo-1], islemTipleri[item[2]], count, calculateSubeValue(item, subeNo)])
+    subeValues = []
+    for item in newSubeList:
+        subeValues.append(item[3])
+    subeValues.sort()
+    for item in newSubeList:
+        index = subeValues.index(item[3])
+        item[3] = index
 
-print_to_new_file("sube1List.txt","")
+    return newSubeList
 
-for dt in sube1user:
-    append_to_file("sube1List.txt", str(dt[0])  + " " + str(islemTipleri[dt[2]]))
+def trainSubeQueue(subeLst):
+    values = []
+    result = []
+    for item in trainData:
+        values.append([item[0],item[1],item[2]])
+        result.append(item[3])
+
+    trainValuesNpArray = array(values)
+    trainResultNpArray = array(result)
+
+    _solver = 'lbfgs'
+    _alpha = 1e-5
+    _hiddenLayerSize = 	(50,50,)
+    clf = MLPClassifier(solver=_solver, alpha=_alpha, hidden_layer_sizes=_hiddenLayerSize)
+    clf.fit(trainValuesNpArray, trainResultNpArray)
+
+    predicted = clf.predict(array([[4, 15, 80]]))
+    print(predicted)
+
+def main():
+    trainSubeData()
+    newSube1List = subeSiraValueHesapla(sube1user, 1)
+    newSube2List = subeSiraValueHesapla(sube2user, 2)
+    trainSubeQueue(newSube1List)
+
+main()
